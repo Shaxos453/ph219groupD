04299ae838fe62cb4237d62e26cd632baa447051
Shaxos453 <shyamuda50@gmail.com>
2020-12-08 03:17:26 +0530

    added sum_pT codes

---
+++ sum_pT_codes/sumpT_toward_6080.C
@@ -0,0 +1,95 @@
+void sumpT_toward_6080()
+{
+  TFile *f = new TFile("13TeV_CR0_RHoff.root"); //opens file 13TeV_CR0_RHoff.root
+  TTree *tree = (TTree*)f->Get("pytree6080"); //opens tree of multiplicity class 60-80
+  Int_t entries = tree->GetEntries(); /*gets no. of entries in the tree (total
+                                        occurences of ntrack)*/
+
+  const Int_t maxTrack = 200; //sets maximum number of tracks to 40
+  //since the max number of tracks in this multiplicity class 20-40 is 40
+
+  Int_t ntrack = 0; //declares and initialises ntrack value to 0
+  Double_t pT[maxTrack]; //declaring arrays of maximum ntrack value length
+  Double_t eta[maxTrack];
+  Double_t phi[maxTrack];
+
+  //setting the branches for each variable we declared previously
+  tree->SetBranchAddress("ntrack",&ntrack);
+  tree->SetBranchAddress("phi",&phi);
+  tree->SetBranchAddress("pT",&pT);
+  tree->SetBranchAddress("eta",&eta);
+
+  //initialising the variables for maximum pT and corresponding ntrack
+  // and corresponding phi value for that maximum pT
+  Double_t maxpt = 0;
+  Double_t phimax = 0;
+  Double_t sum_pT=0;
+  Double_t dphi = 0; //declaring variable to store difference in phi
+
+
+  //creating a canvas for the histogram of size 500px * 500px
+  TCanvas *c2 = new TCanvas("c2", "Average scalar pT sum density for toward region", 750, 400);
+
+  //declaring a histogram with 125 bins, and X axis range from 0 to Pi
+  TH2D * d2_sum_pT = new TH2D("d2N", "Avg. scalar pT sum density",20,0,5,20,0,4);
+  for(Int_t i = 0; i<entries; i++) //going through all ntrack entries
+  {
+    tree->GetEntry(i); //getting all values of ntrack, phi, eta and pT for that entry
+    maxpt = 0;
+    phimax = 0;
+    sum_pT=0;
+    for(Int_t j = 0; j<ntrack; j++) //going through all ntracks in that entry
+    {
+      if(pT[j]>maxpt) //finding max value of pT in the multiplicity class
+      {
+        maxpt = pT[j];
+        phimax = phi[j];
+      }
+    }
+    for(Int_t j = 0; j<ntrack; j++)
+    {
+        dphi = abs(phimax - phi[j]); //dphi is absolute difference between phi[j] and phimax
+        if(dphi>TMath::Pi() && abs(eta[j])<2.5)
+        {
+            dphi = 2*TMath::Pi() - dphi; /*if the difference goes over 2pi then we
+            bring it back to range (0 to pi)*/
+        }
+        if(dphi<TMath::Pi()/3 && abs(eta[j])<2.5)
+        {
+            sum_pT+=pT[j];
+        }
+    }
+    sum_pT=sum_pT/(5*TMath::Pi()/3);
+
+    d2_sum_pT->Fill(maxpt,sum_pT);
+}
+  TProfile* final = d2_sum_pT->ProfileX();
+  final->SetStats(0);
+  Int_t check=0;
+  for(Int_t i=1; i<21; i++)//to get rid of bins for which y=0
+  {
+    if(final->GetBinContent(i) == 0)
+    {
+      check++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  final->GetXaxis()->SetRangeUser(0.25*check, 30);
+  //final->SetMinimum(0);
+  final->SetLineColor(2);
+  final->SetLineWidth(3);
+  final->GetXaxis()->SetLabelSize(0.035);
+  final->GetYaxis()->SetLabelSize(0.035);
+  final->SetTitle("Toward region (60-80)");
+  final->GetXaxis()->SetTitle("pT_{lead} (GeV/c)");
+  final->GetXaxis()->SetTitleSize(0.04);
+  final->GetXaxis()->CenterTitle();
+  final->GetYaxis()->SetTitle("Scalar pT sum density");
+  final->GetYaxis()->SetTitleSize(0.04);
+  final->GetYaxis()->CenterTitle();
+  final->Draw("HIST");
+  //c2->SaveAs("--");
+}
